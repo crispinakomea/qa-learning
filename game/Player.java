5a50ac0bfc157a93f70659b5453256537ad21dc3
Crispin Akomea <Crispinakomea@gmail.com>
2018-04-10 15:10:09 +0100

    Change of values in switch. Addition of symbol.

--- game/Player.java
+++ game/Player.java
@@ -2,60 +2,73 @@ package game;
 
 public class Player {
 
-	private final String name;
-	private final Compass compass;
+	public static final String SYMBOL;
+	private Compass compass;
 	private int[][] position;
 
-	public Player(String name, Compass compass, int[][] position) {
-		this.name = name;
+	static {
+		SYMBOL = "| P |";
+	}
+
+	public Player() {
+
+	}
+
+	public Player(Compass compass, int[][] position) {
 		this.compass = compass;
 		compass.setDistance(position);
 		this.position = position;
 	}
 
-	public String getName() {
-		return name;
+	public Compass getCompass() {
+		return compass;
 	}
 
-	public void checkCompass() {
-		System.out.printf("The dial reads '%.3fm'", compass.getDistance());
-		System.out.println("\n");
+	public void setCompass(Compass compass) {
+		this.compass = compass;
 	}
 
 	public int[][] getPosition() {
 		return position;
 	}
 
+	public void setPosition(int[][] position) {
+		this.position = position;
+	}
+
+	public void checkCompass() {
+		System.out.printf("The dial reads '%.3fm'", compass.getDistance());
+		PrintUtil.newLine();
+	}
+
 	public void move(String direction) {
 		switch (direction) {
-		case "north":
-			position[1][0]++;
-			compass.setDistance(position);
-			checkCompass();
-			break;
-		case "east":
-			position[0][0]++;
-			compass.setDistance(position);
-			checkCompass();
-			break;
-		case "south":
-			position[1][0]--;
-			compass.setDistance(position);
-			checkCompass();
-			break;
-		case "west":
+		case "n":
 			position[0][0]--;
 			compass.setDistance(position);
-			checkCompass();
+			break;
+		case "e":
+			position[1][0]++;
+			compass.setDistance(position);
+			break;
+		case "s":
+			position[0][0]++;
+			compass.setDistance(position);
+			break;
+		case "w":
+			position[1][0]--;
+			compass.setDistance(position);
 			break;
 		default:
+			PrintUtil.print("Invalid direction.");
+			PrintUtil.print("\ttry - n for north, s for south, e for east, w for west");
 			break;
 		}
 	}
 
 	@Override
 	public String toString() {
-		return "Player [name=" + name + ", position=[" + position[0][0] + "," + position[1][0] + "]]";
+		return "Player [position=[" + position[0][0] + "," + position[1][0] + "]]";
 	}
 
 }
