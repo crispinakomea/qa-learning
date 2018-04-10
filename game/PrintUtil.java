0f6a307cb4a6d8ef10d945a83eb5f332833fcd5a
Crispin Akomea <Crispinakomea@gmail.com>
2018-04-10 15:06:49 +0100

    Utility class for printing to console

---
+++ game/PrintUtil.java
@@ -0,0 +1,21 @@
+package game;
+
+public class PrintUtil {
+
+	public static void print(String message) {
+		System.out.println(message);
+	}
+
+	public static void printNewLine(String message) {
+		System.out.println(message + "\n");
+	}
+
+	public static void newLinePrint(String message) {
+		System.out.println("\n" + message);
+	}
+	
+	public static void newLine() {
+		System.out.println("\n");
+	}
+
+}
